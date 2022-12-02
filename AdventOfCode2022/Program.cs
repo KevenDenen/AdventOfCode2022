fcd1561e76a48b2c30744b6359cff1a933afc8f9
Keven Denen <keven.denen@gmail.com>
2022-12-02 09:54:53 -0700

    Add project files.

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,2 @@
+﻿//Future home of Advent of Code 2022 C# Solutions
+Console.WriteLine("Advent of Code 2022 Solutions in C#!");
---
+++ README.md
@@ -0,0 +1,3 @@
+# AdventOfCode2022
+C# Solutions for Advent of Code 2022
+https://adventofcode.com/2022
