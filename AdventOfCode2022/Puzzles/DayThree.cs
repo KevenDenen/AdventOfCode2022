8c1604356754744227f55cabef5256e931fa703e
Keven Denen <keven.denen@gmail.com>
2022-12-03 11:37:06 -0700

    Day Three Part One

---
+++ AdventOfCode2022/Puzzles/DayThree.cs
@@ -0,0 +1,24 @@
+﻿using Helpers;
+
+internal class DayThree
+{
+    private static string fileName = "DayThree.txt";
+    internal static int PartOne()
+    {
+        var file = FileReader.GetWholeFileAsArrayOfLines(fileName);
+        var rucksacks = file.Select(x => x.Chunk(x.Length / 2)).ToList();
+        var matches = rucksacks.Select(x => x.First().Intersect(x.Last()).Single());
+        var priorities = matches.Select(x => CharacterToPriority(x));
+        return priorities.Sum();
+    }
+
+    internal static int PartTwo()
+    {
+        return 0;
+    }
+
+    private static int CharacterToPriority(char character) 
+    {
+        return char.IsLower(character) ? character - 96 : character - 38;
+    }
+}
