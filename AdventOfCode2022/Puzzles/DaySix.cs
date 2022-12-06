d918f888ea7b38e06cc9048568b824e8189ca16b
Keven Denen <keven.denen@gmail.com>
2022-12-05 23:49:54 -0700

    Part one

---
+++ AdventOfCode2022/Puzzles/DaySix.cs
@@ -0,0 +1,28 @@
+﻿using Helpers;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+namespace Puzzles;
+
+internal static class DaySix
+{
+    private static string fileName = "DaySix.txt";
+    internal static int PartOne()
+    {
+        var input = FileReader.GetWholeFileAsString(fileName);
+        var startOfPacket = 0;
+        for (int i = 4; i < input.Length && startOfPacket == 0; i++)
+        {
+            var slider = new HashSet<char>(input.Skip(i-4).Take(4));
+            if (slider.Count <= 3)
+                continue;
+            startOfPacket = i;
+        }
+        return startOfPacket;
+    }
+
+    internal static object PartTwo()
+    {
+        return 0;
+    }
+}
